82a192f91fbc115ce23e556afe9ba2b0917e3b45
kjohna <43828763+kjohna@users.noreply.github.com>
2019-02-11 14:29:23 -0700

    install dependencies, setup server, POST working

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // implement your API here
+
+const express = require('express');
+const db = require('./data/db.js');
+
+const server = express();
+
+server.use(express.json());
+
+// routes
+server.get('/', (req, res) => {
+  res.send('<h2>Server says Hello</h2>');
+});
+
+// POST - create a user with request body
+server.post('/users', (req, res) => {
+  const user = req.body;
+
+  if (!user.name || !user.bio) {
+    res.status(400).json({ success: false, errorMessage: "Please provide name and bio for the user." });
+  }
+
+  db.insert(user)
+    .then( userID => {
+      res.status(201).json({ success: true, userID});
+    })
+    .catch(() => {
+      res.status(500).json({ success: false, error: "There was an error while saving the user to the database" });
+    });
+});
+
+
+// config port
+server.listen(5000, () => {
+  console.log('\n** Server listening on port 5000 **\n');
+});;
